e63dcf5590f27bf87ba15d1302a00888c3b234c0
Shaktiswarup07 <96374586+Shaktiswarup07@users.noreply.github.com>
2023-02-19 16:57:20 +0530

    Added solution - LeetHub

--- Max Min - GFG/max-min.cpp
+++ Max Min - GFG/max-min.cpp
@@ -10,23 +10,32 @@ using namespace std;
 class Solution
 {
    public:
-   int maxi=INT_MIN,mini=INT_MAX;
-   void MaxMin(int a[], int n,int l,int r){
+//   int maxi=INT_MIN,mini=INT_MAX;
+   pair<int,int> MaxMin(int a[], int n,int l,int r){
        if(r==l){
         //   cout<<a[r]<<" ";
-           maxi=max(maxi,a[r]);
-           mini=min(mini,a[r]);
-           return;
+           return {a[r],a[l]};
        }
        int mid=l+(r-l)/2;
-       MaxMin(a,n,l,mid);
-       MaxMin(a,n,mid+1,r);
+       pair<int,int>mami1;
+       pair<int,int>mami2;
+       mami1 = MaxMin(a,n,l,mid);
+       mami2 = MaxMin(a,n,mid+1,r);
+       int maxi,mini;
+       if(mami1.first<mami2.first) maxi=mami2.first;
+       else maxi=mami1.first;
+       if(mami1.second<mami2.second) mini=mami1.second;
+       else mini=mami2.second;
+       return {maxi,mini};
+       
+       
    }
     int findSum(int A[], int N)
     {
     	//code here.
-    	MaxMin(A,N,0,N-1);
-    	return maxi+mini;
+    	pair<int,int>mp=	MaxMin(A,N,0,N-1);
+    
+    	return mp.first+mp.second;
     }
 
 };
