be3e9b7d7b7ac515c7c05deb821ba27ca0524aad
Shaktiswarup07 <96374586+Shaktiswarup07@users.noreply.github.com>
2023-05-08 16:49:37 +0530

    Added solution - LeetHub

--- Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -12,34 +12,31 @@ class Solution {
 public:
     int findMidSum(int ar1[], int ar2[], int n) {
             // code here 
-            int ans=0;
-            int i = 0 , j = 0;
-            int k=0;
-            int p=0;
-            while(i< n && j < n){
-                if(ar1[i]<ar2[j]){
-                    p=ar1[i];
-                    i++;
-                    k++;
-                }
-                else{
-                    p=ar2[j];
-                    j++;
-                    k++;
-                }
-                if(k==n || k==n+1) ans+=p;
+        //     if(nums1.size()>nums2.size()){
+        //     return findMedianSortedArrays(nums2,nums1);
+        // }
+        int n1=n,n2=n;
+        int low=0,high=n;
+        while(low<=high){
+            int cut1=(low+high)/2;
+            int cut2=((n1+n2+1)/2)-cut1;
+
+            int l1=cut1==0?INT_MIN:ar1[cut1-1];
+            int l2=cut2==0?INT_MIN:ar2[cut2-1];
+            int r1=cut1==n1?INT_MAX:ar1[cut1];
+            int r2=cut2==n2?INT_MAX:ar2[cut2];
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0) return (max(l1,l2)+min(r1,r2));
+                else return max(l1,l2);
             }
-            while(i<n){
-                p=ar1[i++];
-                k++;
-                if(k==n || k==n+1) ans+=p;
+            else if(l1>r2){
+                high=cut1-1;
             }
-            while(j<n){
-                p=ar2[j++];
-                k++;
-                if(k==n || k==n+1) ans+=p;
+            else if(l2>r1){
+                low=cut1+1;
             }
-            return ans;
+        }
+        return 0;
     }
 };
 
